789d41b05852f82b6ecd622b783cb6ab9998b044
i3-code <i3pobox@gmail.com>
2020-09-10 14:19:44 +0300

    feat: add Towel Sort solution

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 
 // You should implement your task here.
 
-module.exports = function towelSort (matrix) {
-  return [];
+module.exports = function towelSort(matrix) {
+  let res = [];
+  for (let i in matrix) {
+    res = (i % 2 == 0) ? res.concat(matrix[i]) : res.concat(matrix[i].reverse());
+  }
+  return res;
 }
