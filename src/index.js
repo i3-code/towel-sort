91d8a8ce836abf642c8db19152c18817d7cd0c53
i3-code <i3pobox@gmail.com>
2020-10-14 10:21:20 +0300

    refactor: improve codestyle

--- src/index.js
+++ src/index.js
@@ -2,9 +2,6 @@
 // You should implement your task here.
 
 module.exports = function towelSort(matrix) {
-  let res = [];
-  for (let i in matrix) {
-    res = (i % 2 == 0) ? res.concat(matrix[i]) : res.concat(matrix[i].reverse());
-  }
-  return res;
+    if (!matrix) return [];
+    return matrix.map((n, i) => (i % 2) ? n.reverse() : n).flat();
 }
